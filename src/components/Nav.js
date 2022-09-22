cc2a62731540b358204caadda3de2f3969bbe427
Austin Greer <agreer.ag12@gmail.com>
2022-09-21 21:01:47 -0400

    implemented responsive styles for Nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,8 +1,16 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Nav() {
   return (
-    <div className='nav-container'>Nav</div>
+    <div className='nav-container'>
+      <Link to='/' className='nav-header'>React Shopr</Link>
+
+      <div id='nav-links-wrapper'>
+        <Link to='/products'>Products</Link>
+        <Link to='/checkout'>Cart</Link>
+      </div>
+    </div>
   )
 }
 
